ac1a42e2b0cf67be13dcbf9aead522ab57c79d7b
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-24 17:56:15 +0500

    learned about the destructuring of arrays and and the assigning of the variables to the elements of arrys

--- script.js
+++ script.js
@@ -44,4 +44,34 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterItem, mainItem) {
+    return [restaurant.starterMenu[starterItem], restaurant.mainMenu[mainItem]];
+  },
 };
+
+// destructuring of an array into single and separate variables
+const arr = [1, 2, 34, 5]; //there are four elements inside of this array
+const [x, y, z] = arr; //here i am unpacking three elements of array arr
+console.log(x, y, z);
+
+const [item1, item2, item3] = restaurant.categories;
+
+console.log(item1, item2, item3);
+console.log(item3);
+
+//now what if i want to skip element on index 1 and index 2 from categorie array and then destruct the array categories, we'll do it in this way
+let [ele0, , , ele3] = restaurant.categories;
+console.log(ele0, ele3);
+
+//const [ele0, , , ele3] so as this array will be represented in this manner but what if we want to switch the elements and for that we do this
+[ele0, ele3] = [ele3, ele0];
+console.log(ele0, ele3); // hence now they are switched
+
+// created an function of named order inside restaurant method in which the functionality was user input the index numbers for starter and main menu and the those items will be taken as parameters
+restaurant.order(3, 0);
+console.log(restaurant.order(3, 0));
+
+// here what we did is that we destruct the array in which the names of the item of starter and main is now assigned to separate variables
+const [choosenStarter, choosenMain] = restaurant.order(3, 0);
+console.log(choosenStarter, choosenMain);
