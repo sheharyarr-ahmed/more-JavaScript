2ff3e36f2e7cebc9ece79638c8f29a5e97c7fa5a
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-07-08 18:49:17 +0500

    learned about the new data structure SETS

---
+++ sets.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const learningSets = new Set([
+  "corolla",
+  "civic",
+  "land cruiser",
+  "audi",
+  "audi",
+]);
+console.log(learningSets);
+
+learningSets.add("tesla");
+console.log(learningSets);
+
+learningSets.delete("civic");
+console.log(learningSets);
+
+console.log(learningSets.has("corolla"));
+console.log(learningSets.has("civic"));
+console.log(learningSets.size);
+
+// real life use case of sets
+const visitorIPs = [
+  "192.168.0.1",
+  "192.168.0.2",
+  "192.168.0.1",
+  "10.0.0.5",
+  "192.168.0.2",
+  "172.16.254.1",
+  "10.0.0.5",
+  "192.168.0.3",
+];
+
+const uniqueIps = new Set(visitorIPs);
+console.log(uniqueIps);
+console.log(visitorIPs.length);
+console.log(uniqueIps.size);
+
+for (const uip of uniqueIps) {
+  console.log(uip);
+}
+
+// conversion of set into an array by using spread operator
+const uniqueIpsArray = [...new Set(visitorIPs)];
+console.log(uniqueIpsArray);
+
+const nameSet = new Set("sheharyar");
+console.log(nameSet);
