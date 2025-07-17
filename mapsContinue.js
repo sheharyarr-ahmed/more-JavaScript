d4aff944aabdf15d1e48bdc29a786b07d698d116
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-07-17 20:32:30 +0500

    learned about the maps iteration, conversion of object into map and map into array

--- mapsContinue.js
+++ mapsContinue.js
@@ -24,3 +24,34 @@ console.log(Object.entries(objectTrip));
 
 const tripMap = new Map(Object.entries(objectTrip));
 console.log(tripMap);
+
+// an quiz app
+const quizApp = new Map();
+quizApp
+  .set("Question", "What is the capital of Pkaistan")
+  .set(1, "Karachi")
+  .set(2, "Lahore")
+  .set(3, "Islamabad")
+  .set("correct", 3)
+  .set(true, "your answer is correct")
+  .set(false, "your answer is wrong");
+console.log(quizApp.get("Question"));
+
+for (const [key, value] of quizApp) {
+  if (typeof key === "number") {
+    console.log(`Please Choose: ${key}  ${value}`);
+  }
+}
+
+// const answer = Number(prompt("Please enter your answer")); the real one used
+const answer = 3;
+console.log(answer);
+
+console.log(quizApp.get(quizApp.get("correct") === answer));
+
+// now the conversion of map into an array again
+
+const arrayquizApp = [...quizApp];
+console.log([...quizApp.keys()]);
+console.log([...quizApp.values()]);
+console.log(arrayquizApp);
