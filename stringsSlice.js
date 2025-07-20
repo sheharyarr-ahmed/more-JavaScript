66bd2641b90eff111c430bc175d2681352d478f7
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-07-20 05:31:49 +0500

    made an fucntion

---
+++ stringsSlice.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const myName = "I am Sheharyar";
+const fName = "Memon";
+
+// working with the arrays and some of its methods
+console.log(myName.indexOf("S"));
+console.log(myName.lastIndexOf("h")); //for starting from the last//
+console.log(myName[2]);
+console.log(myName.slice(0, 4));
+console.log(myName.slice(-2));
+
+console.log(myName.slice(0, myName.indexOf(" ")));
+
+// making an samll function
+function formatUserInfo(fullName, cardNo) {
+  const upperCase = fullName.toUpperCase();
+  const fourDigits = cardNo.slice(-4);
+  const hidden = "*".repeat(12) + fourDigits;
+
+  console.log(
+    `${upperCase} and Card No is ${fourDigits} and full card number ${hidden}`
+  );
+}
+
+formatUserInfo("Sheharyar Ahmed", "887766");
