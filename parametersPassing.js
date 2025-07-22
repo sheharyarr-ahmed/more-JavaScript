f24fd62bc1cd1c0639c960583eb4294532ed60f3
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-07-22 06:39:30 +0500

    learned about the pasing by reference and passing by value

---
+++ parametersPassing.js
@@ -0,0 +1,24 @@
+"use strict";
+// this file highlights the issue of passing by value(primitives) and pasing by reference(non primitives like arrays, objects and functions)
+//  there is difference whem an string or an primitive value is passed in an function it remains same/ its just copied but when an object is passed in an function it alters the original state
+const airline = "fly jinnah";
+const shery = {
+  name: "sheharyar ahmed",
+  nationality: "pakistani",
+};
+
+const info = function (airline, quom) {
+  airline = "air sial";
+  quom.name = "as an" + quom.name;
+
+  if (quom.nationality === "pakistani") {
+    alert("checked in");
+  } else {
+    alert("invalid nationality");
+  }
+};
+info(airline, shery);
+console.log(airline);
+console.log(shery);
+
+// this function is an perfect exampple of difference between passing of primitives thorugh an functiona and passing of objects as an function
